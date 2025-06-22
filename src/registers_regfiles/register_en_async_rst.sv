14e29e8e28807cc425dc6ca756dbc7bdf5e12524
Marco Spaziani Brunella <marco@hyperable.io>
2025-06-22 15:25:24 -0700

    Updated License

--- src/registers_regfiles/register_en_async_rst.sv
+++ src/registers_regfiles/register_en_async_rst.sv
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//     Copyright (c) 2025 Siliscale Inc.
+//     Copyright (c) 2025 Siliscale Consulting, LLC
 // 
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
